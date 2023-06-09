30034ace672500720794b9907a3dc6ceaf56ba84
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-06-09 14:02:11 +0500

    passing children

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "London", "Paris", "Tokyo"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }

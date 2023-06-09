a3326d6627c40942caa2e742738933fd91d1b100
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-06-09 14:17:51 +0500

    building a button component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,11 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button color="secondary" onClick={() => console.log("clicked")}>
+        My Button
+      </Button>
     </div>
   );
 }
